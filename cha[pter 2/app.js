de02a0353b90ec5a9252140379c8f011c461c109
Areebusman27 <155239399+Areebusman27@users.noreply.github.com>
2024-02-23 19:40:11 +0500

    assignment update

--- cha[pter 2/app.js
+++ cha[pter 2/app.js
@@ -3,21 +3,21 @@ var userName = 'userName'
 // Q2
 var fullName = ' AREEB USMAN'
 // Q3
-var message = 'hello world'
+var message = 'Hello World'
 alert(message)
 // Q4
-var name = 'areeb usman'
+var name = 'Areeb Usman'
 var age = 18
-var profession = 'Certified web developer'
+var profession = 'Certified Web Developer'
 alert(name)
 alert(age)
 alert(profession)
 // Q5
-var Q5 = 'PIZZA \nIZZA \n ZA \nZA \nA'
+var Q5 = 'PIZZA \nIZZA \nZZA \nZA \nA'
 alert(Q5)
 // Q6
 var email = 'example@example.com'
-alert('my email is ' + email) 
+alert('My email is ' + email) 
 // Q7
 var book = 'SMARTER WAY TO LEARN TO JAVASCRIPT'
 alert('My book is ' + book)
--- cha[pter 3/app.js
+++ cha[pter 3/app.js
@@ -1,3 +1,3 @@
 // Q1
 var age = 18
-alert('i am '+ age + "years old")   
+alert('I am '+ age + "years old")   
