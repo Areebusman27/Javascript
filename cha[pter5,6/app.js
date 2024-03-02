25703af050a0461efd6e8513e8445fc9f659cb6a
Areebusman27 <155239399+Areebusman27@users.noreply.github.com>
2024-03-03 00:56:59 +0500

    assignment update

---
+++ cha[pter5,6/app.js
@@ -0,0 +1,16 @@
+var num = prompt('Enter the number')
+document.write(`<h1>Table of ${num}</h1>`)
+document.write(`<h1>${num} * 1 = ${num *1}`)
+document.write(`<h1>${num} * 2 = ${num *2}`)
+document.write(`<h1>${num} * 3 = ${num *3}`)
+document.write(`<h1>${num} * 4 = ${num *4}`)
+document.write(`<h1>${num} * 5 = ${num *5}`)
+document.write(`<h1>${num} * 6 = ${num *6}`)
+document.write(`<h1>${num} * 7 = ${num *7}`)
+document.write(`<h1>${num} * 8 = ${num *8}`)
+document.write(`<h1>${num} * 9 = ${num *9}`)
+document.write(`<h1>${num} * 10 = ${num *10}`)
+
+
+
+
